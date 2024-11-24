9646f856d050e90bcd5e30f3d94ba5e2cd2d7e90
Nicolás Mateo Macías <55945912+NMateoo@users.noreply.github.com>
2024-11-24 17:10:08 +0100

    Mejoras varias

--- porfolio/tailwind.config.js
+++ porfolio/tailwind.config.js
@@ -1,24 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
     "./src/**/*.{html,ts}",
+    "./index.html"
   ],
   darkMode: "class",
   theme: {
     extend: {
-      keyframes: {
-        typing: {
-          '0%': { width: '0%' },
-          '100%': { width: '100%' },
-        },
-        blink: {
-          '0%, 100%': { borderColor: 'transparent' },
-          '50%': { borderColor: 'black' },
-        }
-      },
-      animation: {
-        typing: 'typing 2s steps(25, end), blink 0.75s step-end infinite',
-      },
+      
     },
   },
   plugins: [],
